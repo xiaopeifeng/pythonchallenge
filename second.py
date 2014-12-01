2046ea73d094fcd4234e78c78f8b05b0846714a5
xiaopeifeng <xiaopeifenng@gmail.com>
2014-12-01 20:42:55 +0800

    second

---
+++ second.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+from string import maketrans
+import string
+
+intab = "abcdefghijklmnopqrstuvwxyz"
+outtab = "cdefghijklmnopqrstuvwxyzab"
+trantab = maketrans(intab, outtab)
+str = "g fmnc wms bgblr rpylqjyrc gr zw fylb. rfyrq ufyr amknsrcpq ypc dmp. bmgle gr gl zw fylb gq glcddgagclr ylb rfyr'q ufw rfgq rcvr gq qm jmle. sqgle qrpgle.kyicrpylq() gq pcamkkclbcb. lmu ynnjw ml rfc spj. "
+print str.translate(trantab)
+strurl = "map"
+print strurl.translate(trantab)
